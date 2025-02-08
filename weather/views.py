3debe4c266776154e86a68e38d82387d83d28afa
camarahacks <gustavo-camara@outlook.com>
2025-02-08 13:44:15 -0300

    Paginas e consultas da api

--- weather/views.py
+++ weather/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from weather.models import Cidade
+from weather.utils import get_weather 
 
 def index(request):
     cidades = Cidade.objects.all()
     return render(request, "weather/index.html", {"cidades": cidades})
 # Create your views here
 
+def previsao(request, pk):
+    cidade = get_object_or_404(Cidade, pk=pk)
+    prev = get_weather(cidade.lat, cidade.long)
+    return render(
+        request, 
+        "weather/previsao.html", 
+        {
+         "cidade":cidade,
+         "previsao": prev
+
+         })
